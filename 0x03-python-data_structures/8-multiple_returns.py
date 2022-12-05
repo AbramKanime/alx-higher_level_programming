9b9e829ae7d0208a31bad6f800f84d72c9f5749c
AbramKanime <abramkanime@gmail.com>
2022-12-05 11:45:01 +0100

    Hot fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,9 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0]
-    tup = (length, first_char)
+
     if length == 0:
-        first_char = None
+        tup = (length, None)
+    else:
+        tup = (length, first_char)
     return(tup)
