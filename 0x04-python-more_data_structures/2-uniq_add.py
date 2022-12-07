ec4b76a281ec7a6b5a9fbdbd093badf533a37257
AbramKanime <abramkanime@gmail.com>
2022-12-07 16:08:18 +0100

    Hot fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 def uniq_add(my_list=[]):
     un_set = set(my_list)
-    my_list = list(un_set)
     sum_all = 0
-    for i in range(len(my_list)):
-        sum_all += my_list[i]
+    for i in un_set:
+        sum_all += i
     return sum_all
