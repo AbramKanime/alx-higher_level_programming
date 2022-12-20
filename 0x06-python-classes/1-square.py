c3281420df9a08566617e2b0d2bef3d5fc0d07c2
AbramKanime <abramkanime@gmail.com>
2022-12-20 23:25:31 +0100

    Added new files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """A class square that defines a square"""
+    def __init__(self, size):
+        """Initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of square
